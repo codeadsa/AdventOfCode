89dd5aca736cf46857a8a3bd3028f982a0f13845
asa <andyahnn@gmail.com>
2021-12-01 21:58:59 -0800

    better parsing lines into arrays

--- src/2021/Day02.py
+++ src/2021/Day02.py
@@ -17,15 +17,16 @@ for data in dataset:
         y -= int(data[3:])
 print(f'Part1: {x * y}') #1580000
 
-x, y, aim =0, 0, 0
-dataset = parse_file('Day02_input.txt')
-for data in dataset:
-    if data.startswith("down"):
-        aim += int(data[5:])
-    elif data.startswith("forward"):
-        x += int(data[8:])
-        y += aim * int(data[8:])
-    else:
-        aim -= int(data[3:])
-print(f'Part2: answer = {x * y}') #1251263225
+horiz, depth, aim =0, 0, 0
+with open('Day02_input.txt') as f:
+    cmds = [(cmd, int(amt)) for cmd, amt in [line.strip().split() for line in f]]
+    for cmd, x in cmds:
+        if cmd == "down":
+            aim += x
+        elif cmd == "forward":
+            horiz += x
+            depth += aim * x
+        else:
+            aim -= x
+print(f'Part2: answer = {horiz * depth}') #1251263225
 
